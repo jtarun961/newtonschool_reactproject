f47e19491c7cc13de7ca45e49e582ce808087b09
Tarun Jindal <jtarun961@gmail.com>
2022-04-11 23:13:43 +0530

    Update Jurassic.js

--- src/Jurassic.js
+++ src/Jurassic.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Jurassic(){
     return(
         <div className='logo'>
-        <img src="images/jurassic.jpg" alt="jurassic Park" />
+        <img src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_.jpg" alt="jurassic Park" />
         <h1>#5-<br />Jurassic<br />World<br></br>(2015)</h1>
         <p>Distributor:Universal<br />Pictures</p>
         <p>Amount:<br />$1,671,713,208</p>
