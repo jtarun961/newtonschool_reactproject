3cc1eb3a0544db3af7acb6c1b935e7248da597a4
Tarun Jindal <jtarun961@gmail.com>
2022-04-11 23:02:47 +0530

    Updates

---
+++ precache-manifest.41c429047f6a7b3088a167e0a4bb4761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1422d8729ab40202182",
+    "url": "/movielist/static/css/main.eb96ca5c.chunk.css"
+  },
+  {
+    "revision": "a1422d8729ab40202182",
+    "url": "/movielist/static/js/main.a1422d87.chunk.js"
+  },
+  {
+    "revision": "935b0251a626769e024b",
+    "url": "/movielist/static/js/1.935b0251.chunk.js"
+  },
+  {
+    "revision": "98630b08fef7cc82835d",
+    "url": "/movielist/static/js/2.98630b08.chunk.js"
+  },
+  {
+    "revision": "91a1ece05c7c4bc0409a",
+    "url": "/movielist/static/js/runtime~main.91a1ece0.js"
+  },
+  {
+    "revision": "16e3f984b139530ca70b0afaef5412a0",
+    "url": "/movielist/index.html"
+  }
+];
