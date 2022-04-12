55880bfbaf4b2f3ccedbe1032fe0e65831bae708
Tarun Jindal <jtarun961@gmail.com>
2022-04-12 15:06:50 +0530

    Update Star.js

--- src/Star.js
+++ src/Star.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Star(){
     return(
         <div className='logo'>
-        <img src="images/starwars.jpg" alt="" />
+        <img src="https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372" alt="" />
         <h1>#3-Star <br /> Wars: The <br /> Force <br /> Awakens <br></br>(2015)</h1>
         <p>Distributor:Walt <br /> Disney Studios Motion <br /> Pictures</p>
         <p>Amount: <br />$2,068,223,624</p>
