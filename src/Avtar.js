5022e6611590ec5a949ab1f3e013dd2c65528898
Tarun Jindal <jtarun961@gmail.com>
2022-04-12 15:05:58 +0530

    Update Avtar.js

--- src/Avtar.js
+++ src/Avtar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Avtar(){
     return(
         <div className='logo'>
-        <img src="images/avtar.jpg" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/en/d/d6/Avatar_%282009_film%29_poster.jpg" alt="" />
         <h1>#1-Avatar <br></br>(2009)</h1>
         <p>Distributor:20th <br></br> Century Fox</p>
         <p>Amount: <br />$2,787,965,087a</p>
