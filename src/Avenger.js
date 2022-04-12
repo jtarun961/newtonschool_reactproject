5ec6108d2c2eac3b0fa5645eed39abc94ebf7e0c
Tarun Jindal <jtarun961@gmail.com>
2022-04-12 15:04:28 +0530

    Update Avenger.js

--- src/Avenger.js
+++ src/Avenger.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Avenger(){
     return(
         <div className='logo'>
-        <img src="images/wallpapersden.com_avengers-infinity-war-international-poster_1920x1200.jpg" alt="" />
+        <img src="https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0%2C0%2C540%2C810" alt="" />
         <h1>#4-<br />Avengers:<br />infinity wars <br></br>(2018)</h1>
         <p>Distributor:Walt <br /> Disney Studios Motion <br /> Pictures</p>
         <p>Amount: <br />$2,048,359,754</p>
