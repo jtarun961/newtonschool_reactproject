33deadc1903ff5c82d6f1a18f7de4a8e5c3026b6
Tarun Jindal <jtarun961@gmail.com>
2022-04-12 15:07:26 +0530

    Update Titanic.js

--- src/Titanic.js
+++ src/Titanic.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Titanic(){
     return(
         <div className='logo'>
-        <img src="images/titanic.jpg" alt="" />
+        <img src="https://pbs.twimg.com/profile_images/1213512365624328192/bKV4_9Ex.jpg" alt="" />
         <h1>#2-Titanic <br></br>(1997)</h1>
         <p>Distributor:20th <br /> Century Fox</p>
         <p>Amount: <br />2,187,463,944</p>
